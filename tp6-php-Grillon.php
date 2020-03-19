b92be36cc3e039cb360d1487664be76cfad452e9
Theo-Grillon <theo.grillon@laposte.net>
2020-03-19 11:45:37 +0100

    exo1

---
+++ tp6-php-Grillon.php
@@ -0,0 +1,10 @@
+<?php
+class FootballTeam{
+    public $name, $nbTitles;
+    function display(){
+        echo "L'équipe ".$this->name."a ".$this->nbTitles." titres";
+    }
+}
+$atl= new FootballTeam();
+$atl->name="Falcons";
+$atl->nbTitles=8;

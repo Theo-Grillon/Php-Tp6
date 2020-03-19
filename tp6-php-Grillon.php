8f4280c6197f1f1f73772aa72aa1dd40f7788270
Theo-Grillon <theo.grillon@laposte.net>
2020-03-19 12:05:49 +0100

    exo1v4

--- tp6-php-Grillon.php
+++ tp6-php-Grillon.php
@@ -2,6 +2,11 @@
 class FootballTeam{
     //public $name, $nbTitles; pour exo1
     private $name, $nbTitles;
+    const Txt="Nombre d'équipes";
+    static $inc=0;
+    static function getTeamNum(){
+
+    }
     function display(){
         echo "L'équipe ".$this->name." a ".$this->nbTitles." titres";
     }
@@ -17,12 +22,21 @@ class FootballTeam{
     function setNbTitle($num){
         $this->nbTitles=$num;
     }
+    function __construct($name, $num){
+        $this->name=$name;
+        $this->nbTitles=$num;
+    }
+    function __destruct(){
+        echo "Destructor";
+    }
 }
-$atl= new FootballTeam(); /*pour exo1
+/*$atl= new FootballTeam(); pour exo1
 $atl->name="Atlanta Falcons";
 $atl->nbTitles=8;
 $atl->display();
-*/
+
 $atl->setName("Atlanta Falcons");
 $atl->setNbTitle(8);
+$atl->display();*/
+$atl= new FootballTeam("Atlanta Falcons", 8);
 $atl->display();

0c6ebae731aef1e99059fafe8a6152b6c98c6527
Theo-Grillon <theo.grillon@laposte.net>
2020-03-24 17:27:17 +0100

    exo3v1

--- formulaire.php
+++ formulaire.php
@@ -3,13 +3,26 @@ class Formulaire{
     private $form="";
     public function __construct($filename)
     {
-        $this->form.="<form action='$filename' method='post'><br>";
+        $this->form.="<form action='$filename' method='get'><br>";
     }
-    function addTextZone($text){
-        $this->form.="<b>".$text."</b><input type='text'><br>";
+    function addTextZone($text=""){
+        $this->form.="<b>".$text."</b><input type='text' name='$text'><br>";
     }
     function addButton(){
         $this->form.="<input type='submit' value='Envoi'><br>";
+    }
+    function addSelect($text){
+        $this->form.="$text"."<select name='age'>
+        <option value=''>--Age--</option>
+        <option value='0-20'>0-20</option>
+        <option value='20-40'>24-40</option>
+        <option value='41-60'>41-60</option>
+        <option value='60+'>60 et +</option>
+    </select><br>";
+    }
+    function addGenderChoice(){
+        $this->form.="Monsieur<input type=\"checkbox\" name=\"Gender\">
+    Madame<input type=\"checkbox\" name=\"Gender\"><br>";
     }
     function getForm(){
         $this->form.="</form>";
--- testFormulaire.php
+++ testFormulaire.php
@@ -1,8 +1,28 @@
+<h2>Exercice2</h2>
 <?php
 include "formulaire.php";
 
 $form=new Formulaire("testFormulaire.php");
-$form->addTextZone("Votre nom :");
-$form->addTextZone("Votre code :");
+$form->addTextZone("Nom:");
+$form->addTextZone("Prenom");
+$form->addTextZone("Mail:");
+$form->addSelect("Age:");
+$form->addGenderChoice();
 $form->addButton();
 echo $form->getForm();
+
+class Recuperator{
+    private $valtab=[];
+    public function __construct($tab)
+    {
+        foreach ($tab as $value){
+            $valtab[]=$value;
+        }
+        echo $valtab;
+    }
+}
+
+if ($_POST!=null){
+    $test=new Recuperator($_POST);
+}
+?>
